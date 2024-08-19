8a8400a89ab143dee48fd7ce1292e400f4375b9f
Navyum <36869790+Navyum@users.noreply.github.com>
2024-08-20 00:41:24 +0800

    Update sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,6 +3,9 @@ import { sidebar } from "vuepress-theme-hope";
 // 图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#%E8%AE%BE%E7%BD%AE%E5%9B%BE%E6%A0%87
 // https://fontawesome.com/search?m=free&o=r
 
+
+export default sidebar({})
+
 /* 全关闭
 export default sidebar({
   "": [    
