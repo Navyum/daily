09bc7d41aeef050ed1533510b03effe94395ba8b
Navyum <36869790+Navyum@users.noreply.github.com>
2025-03-11 15:58:29 +0800

    Update product_hunt_list_to_md.py

--- scripts/product_hunt_list_to_md.py
+++ scripts/product_hunt_list_to_md.py
@@ -40,7 +40,6 @@ class Product:
         # 添加更多请求头信息
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Origin": "https://decohack.com",
             "Accept-Language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
             "Connection": "keep-alive"
         }
